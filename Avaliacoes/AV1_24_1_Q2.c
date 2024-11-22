fb419c8707260bc986fe6bd7ac8d45419a6d1bee
rafcaos1768 <160551377+rafcaos1768@users.noreply.github.com>
2024-11-22 23:20:38 +0000

    atualizando

--- Avaliacoes/AV1_24_1_Q2.c
+++ Avaliacoes/AV1_24_1_Q2.c
@@ -7,7 +7,69 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 
-int quebraData(char data){
+typedef struct TipoData{
+    char dataInvertida[11];
+    int validade;
+
+} TipoData; 
+
+// //Utilizar um retorno do tipoData para a função principal receber
+// TipoData quebraData(char data){
+//     TipoData mhData; //Necessario declarar o tipo minhadata para o retorno da função
+//     char sDia[3]; //Delcara com um esoaco a mais para nao dar stack overflow 
+//     char SMes[3];
+//     char SAno[5]; 
+
+//     for(int i =0; data[i] != '/'; i++){ //atribuicao cate achar o '/'
+//         sDia[i] = data[i];
+//     }
+
+//     // utilizar o i para colocar o '\0' e indicar o fim da string 
+//     if(i == 1 || i == 2){
+//         sDia[i] = '\0'; 
+//     } else {
+//         return mhData.validade = 0; //RETORNA UM VALOR INVALIDO 
+//     }
+
+//     //declar j para continuar aonde parou o '/'
+//     int j = i +1 
+//     i = 0; //reiniciar variavel i
+
+//     for(;data[j]!= '/'; j++){ //Mesmo esquema de atribuição || Interesessante ressaltar que o for pode ser feito sem a parte de declarar variavel 
+//         sMes[i] = data[j];
+//         i++;
+//     }
+
+//     if(i==1 || i==2 ){ // a variavel i eh utilizada pois ela mantem o controle do da variavel mes 
+//         sMes[j] = '\0';
+//     }else {
+//         return mhData.validade = 0; //RETORNA INVALIDO
+//     }
+
+//     int k = j+1; 
+//     i = 0;
+
+//     for(; data[k]!= '/'; k++){
+//         sAno[i]= data[k];
+//         i++;
+//     }
+    
+//     if(i== 2 || i == 4){
+//         SAno[i]=='\0';
+//     } else {
+//         return mhData.validade = 0; //RETORNA INVALIDO
+//     }
+
+//     // for    
+
+//     return mhData.validade = 1; // retorna valido 
+
+
+// }
+
+//Utilizar um retorno do tipoData para a função principal receber
+TipoData quebraData(char data){
+    TipoData mhData; //Necessario declarar o tipo minhadata para o retorno da função
     char sDia[3]; //Delcara com um esoaco a mais para nao dar stack overflow 
     char SMes[3];
     char SAno[5]; 
@@ -20,23 +82,60 @@ int quebraData(char data){
     if(i == 1 || i == 2){
         sDia[i] = '\0'; 
     } else {
-        return 0; //RETORNA UM VALOR INVALIDO 
+        return mhData.validade = 0; //RETORNA UM VALOR INVALIDO 
     }
 
     //declar j para continuar aonde parou o '/'
     int j = i +1 
+    i = 0; //reiniciar variavel i
 
-    for(;sMes[i] == '/', j++){ //Mesmo esquema de atribuição || Interesessante ressaltar que o for pode ser feito sem a parte de declarar variavel 
-        sMes[i] = data[i];
+    for(;data[j]; j++){ //Mesmo esquema de atribuição || Interesessante ressaltar que o for pode ser feito sem a parte de declarar variavel 
+        sMes[i] = data[j];
+        i++;
     }
 
-    if(j==1 || j==2 ){
+    if(i==1 || i==2 ){ // a variavel i eh utilizada pois ela mantem o controle do da variavel mes 
         sMes[j] = '\0';
     }else {
-        return 0; //RETORNA INVALIDO
+        return mhData.validade = 0; //RETORNA INVALIDO
     }
 
     int k = j+1; 
+    i = 0;
+
+    for(; data[k]!= '/'; k++){
+        sAno[i]= data[k];
+        i++;
+    }
+    
+    if(i== 2 || i == 4){
+        SAno[i]=='\0';
+    } else {
+        return mhData.validade = 0; //RETORNA INVALIDO
+    }
+
+
+    ///////////////////////////////////////////////////
+    //inverter string 
+    for(int m=0; SMes[m] != '\0'; m++){
+        mhData.dataInvertida[m]=sMes[m];
+    }  
+
+    mhData.dataInvertida [m]= '/';
+    m ++; 
+
+    for(int n=0; SMes[n] != '\0'; n++){
+        mhData.dataInvertida[m]= 
+    }
+
+    return mhData.validade = 1; // retorna valido 
+
 
 }
+
+
+
+
+
+
 
