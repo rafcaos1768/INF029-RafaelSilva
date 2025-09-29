64f98203bebb22e3a461bd1c0de3e8d9cdb391fb
Rafael Silva <160551377+rafcaos1768@users.noreply.github.com>
2025-09-29 22:44:45 +0000

    cadastro de aluno

--- ProjetoEscola/main.c
+++ ProjetoEscola/main.c
@@ -1,32 +1,96 @@
 #include <stdlib.h> 
+#include <stdio.h> 
+#include <stdbool.h> 
+#define MAX 100 
 
-void introduction()
-void menuStudent()
-void menuTeacher()
-void menuSubject()
-void menuReports();
+typedef struct student {
+  char name[100];
+  char gender;
+  int age; 
+  char id[100];
+  char birthdate[100];
+  int registration;
+  int status;
+} student;
+
+
+void introduction(){
+    printf("=====================================\n"); printf(" Bem-vindo ao Projeto Escola!\n"); printf(" Sistema de Cadastro Escolar\n"); printf("=====================================\n\n");
+}
+
+
+
+/*ALUNO*/
+int menuStudent(){
+    int choice; 
+    printf("=== Informe a operação desejada==\n");
+    printf("1-Cadastrar\n");
+    scanf("%d", &choice);
+
+    return choice; 
+}
+
+bool CadastrarAluno(student baseOfStudents[], int qtd){
+    printf("digite o nome do aluno\n"); 
+    scanf("%s", baseOfStudents[qtd].name);
+
+    printf("digite a idade do aluno\n");
+    scanf("%d", &baseOfStudents[qtd].age);
+
+    printf("digite o CPF do aluno\n");
+    scanf("%s", baseOfStudents[qtd].id);
+
+    printf("digite o aniversario do aluno\n");
+    scanf("%s", baseOfStudents[qtd].birthdate);
+
+    printf("digite a matricula do alunoz\n");
+    scanf("%d", &baseOfStudents[qtd].registration);
+
+    return true; 
+}
+
+
+// void menuTeacher();
+// void menuSubject();
+// void menuReports();
+
 
 
 void main(){
     introduction();
 
+    student baseOfStudents[MAX];
+    int CountStudents = 0;
     int choice = -1;
-    while(choice!=0){
+
+    while(1){
         printf("Escolha o modulo");
-        printf("0- Sair\n1- Aluno\n2- Professor\n3- Disciplina\n 4-Relatorios ");
+        printf("\n0- Sair\n1- Aluno\n2- Professor\n3- Disciplina\n4-Relatorios\n");
         scanf("%d", &choice);
 
         if(choice == 1 ){
-            menuStudent();
+            int choiceStudent = menuStudent();
+            if(choiceStudent==1){
+                bool studentOperation = CadastrarAluno(baseOfStudents, CountStudents);
+
+                if(studentOperation== true ){
+                    printf("\nAluno cadastrado com sucesso\n");
+                }else{
+                    printf("ERRO");
+                }
+            }
 
         }else if (choice == 2){
-            menuTeacher();
+            // menuTeacher();
         }else if (choice == 3){
-            menuSubject();
+            // menuSubject();
         }else if(choice == 4){
-            menuReports();
+            // menuReports();
+        }else if(choice==0 ){
+           printf("até logo!");
+           break;
         }else{
-            printf("opção invalida!")
+             printf("opção invalida!");
         }
     }
 
