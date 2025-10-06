c5d0fb45df30426096e01238a210015c5482cd05
Rafael Silva <160551377+rafcaos1768@users.noreply.github.com>
2025-10-06 23:23:06 +0000

    Processo de listagem de alunos nao concluido

--- ProjetoEscola/main.c
+++ ProjetoEscola/main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h> 
 #include <stdio.h> 
 #include <stdbool.h> 
+#include <string.h>
 #define MAX 100 
 
 typedef struct student {
@@ -54,18 +55,64 @@ bool exlcuirAluno(student baseOfStudents[]){
 
 }
 
-//buscar aluno 
+//buscar aluno
 // void menuTeacher();
 // void menuSubject();
 // void menuReports();
 
+//*FIM ALUNO */
 
+//REPORTS 
+
+int menuReports(){
+    int choice; 
+    printf("=== Informe a operação desejada==\n");
+    printf("1-Relatorio dos Alunos\n2- Relatorio dos Professores\n3- Relatorios das Disciplinas\n4-Sair");
+    scanf("%d", &choice);
+
+    return choice; 
+}
+
+int menuReportsStudents(){
+    int choice; 
+    printf("=== Informe a operação desejada==\n");
+    printf("1-Listar Alunos em ordem Alfabetica\n2- Relatorio dos Professores\n3- Relatorios das Disciplinas\n4-Sair");
+    scanf("%d", &choice);
+
+    return choice; 
+}
+
+void listAllStudantsAlphabeticOrder(student baseOfStudents[], int qtd){
+    int index = 0;
+    char names[MAX][MAX];
+    char temp[MAX];
+    int i,j;
+
+    //ordenar nomes em ordem alfabetica usando bubble sort 
+     for ( i = 0 ; i < qtd - 1; i++) {
+        for (j = 0; j < qtd - 1 - i; j++) {
+            if (strcmp(names[j], names[j + 1]) > 0) {
+                strcpy(temp, names[j]);
+                strcpy(names[j], names[j + 1]);
+                strcpy(names[j + 1], temp);
+            }
+        }
+    }
+    //impressao dos nomes
+    printf("\nLista de alunos em ordem alfabetica :\n");
+    while(index != qtd){
+       printf("%s\n", names[index]);
+        index++;
+    }
+}
+
+//END REPORTS 
 
 void main(){
     introduction();
 
-    student baseOfStudents[MAX];
-    int CountStudents = 0;
+    student baseOfStudents[MAX]; // Variavel para armazena-los
+    int CountStudents = 0; // quantidade de estudantes 
     int choice = -1;
 
     while(1){
@@ -80,6 +127,7 @@ void main(){
 
                 if(studentOperation== true ){
                     printf("\nAluno cadastrado com sucesso\n");
+                    CountStudents++;
                 }else{
                     printf("ERRO");
                 }
@@ -95,13 +143,30 @@ void main(){
         }else if (choice == 3){
             // menuSubject();
         }else if(choice == 4){
-            // menuReports();
-        }else if(choice==0 ){
+           
+            int choiceReports=  menuReports();
+            if(choiceReports==1){
+                int choiceMenuReportsStudents = menuReportsStudents();
+                if(choiceMenuReportsStudents==1){
+                    listAllStudantsAlphabeticOrder(baseOfStudents, CountStudents);
+                }
+                
+            }else if (choiceReports ==  2 ){
+
+            }else if(choiceReports==3){
+
+
+            }
+           else if(choiceReports==0 ){ 
            printf("até logo!");
            break;
+           }else{
+            printf("opção invalida!");
+           }
         }else{
-             printf("opção invalida!");
-        }
+            printf("Até logo");
+            break;
+        }    
     }
 
 
